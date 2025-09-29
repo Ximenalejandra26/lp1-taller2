7c67db587adee65a345aca168490eea0e4366f94
Ximena <Ximena.aleja021@outlook.com>
2025-09-28 21:06:11 -0500

    Solucionar problema 2

--- problema2/cliente.py
+++ problema2/cliente.py
@@ -6,27 +6,33 @@ Objetivo: Crear un cliente TCP que envíe un mensaje al servidor y reciba la mis
 
 import socket
 
-# TODO: Definir la dirección y puerto del servidor
+# Definir la dirección y puerto del servidor
+HOST = 'localhost'
+PORT = 9002
 
 # Solicitar mensaje al usuario por consola
 message = input("Mensaje: ")
 
-# TODO: Crear un socket TCP/IP
+# Crear un socket TCP/IP
 # AF_INET: socket de familia IPv4
 # SOCK_STREAM: socket de tipo TCP (orientado a conexión)
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# TODO: Conectar el socket al servidor en la dirección y puerto especificados
+# Conectar el socket al servidor en la dirección y puerto especificados
+cliente.connect((HOST, PORT))
 
 # Mostrar mensaje que se va a enviar
 print(f"Mensaje '{message}' enviado.")
 
-# TODO: Codificar el mensaje a bytes y enviarlo al servidor
+#Codificar el mensaje a bytes y enviarlo al servidor
 # sendall() asegura que todos los datos sean enviados
+cliente.sendall(message.encode())
 
-# TODO: Recibir datos del servidor (hasta 1024 bytes)
+# Recibir datos del servidor (hasta 1024 bytes)
+data = cliente.recv(1024)
 
 # Decodificar e imprimir los datos recibidos
 print("Mensaje recibido: ", data.decode())
 
-# TODO: Cerrar la conexión con el servidor
-
+#Cerrar la conexión con el servidor
+cliente.close()
--- problema2/servidor.py
+++ problema2/servidor.py
@@ -6,31 +6,37 @@ Objetivo: Crear un servidor TCP que devuelva exactamente lo que recibe del clien
 
 import socket
 
-# TODO: Definir la dirección y puerto del servidor
+#Definir la dirección y puerto del servidor
+HOST = ''  # Escuchar en todas las interfaces
+PORT = 9002
 
-# TODO: Crear un socket TCP/IP
+#Crear un socket TCP/IP
 # AF_INET: socket de familia IPv4
 # SOCK_STREAM: socket de tipo TCP (orientado a conexión)
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# TODO: Enlazar el socket a la dirección y puerto especificados
+#Enlazar el socket a la dirección y puerto especificados
+servidor.bind((HOST, PORT))
 
-# TODO: Poner el socket en modo escucha
+#Poner el socket en modo escucha
 # El parámetro define el número máximo de conexiones en cola
+servidor.listen()
 
 # Bucle infinito para manejar múltiples conexiones (una a la vez)
 while True:
 
     print("Servidor a la espera de conexiones ...")
     
-    # TODO: Aceptar una conexión entrante
+    #Aceptar una conexión entrante
     # accept() bloquea hasta que llega una conexión
     # conn: nuevo socket para comunicarse con el cliente
     # addr: dirección y puerto del cliente
-    
+    conn, addr = servidor.accept()
     print(f"Conexión realizada por {addr}")
 
-    # TODO: Recibir datos del cliente (hasta 1024 bytes)
-    
+    #Recibir datos del cliente (hasta 1024 bytes)
+    data = conn.recv(1024)
+
     # Si no se reciben datos, salir del bucle
     if not data:
         break
@@ -38,7 +44,7 @@ while True:
     # Mostrar los datos recibidos (en formato bytes)
     print("Datos recibidos:", data)
     
-    # TODO: Enviar los mismos datos de vuelta al cliente (echo)
-    
-    # TODO: Cerrar la conexión con el cliente actual
-
+    #Enviar los mismos datos de vuelta al cliente (echo)
+    conn.sendall(data)
+    #Cerrar la conexión con el cliente actual
+    conn.close()
